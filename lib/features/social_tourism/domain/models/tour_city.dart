6bb53ace23e82dcfd1a86616c6d33d6a19b1fca0
sekimoto-takaharu <taka20000713@yahoo.co.jp>
2026-03-27 00:11:33 +0900

    暫定での完成#11

---
+++ lib/features/social_tourism/domain/models/tour_city.dart
@@ -0,0 +1,19 @@
+class TourCity {
+  const TourCity({
+    required this.name,
+    required this.prefecture,
+    required this.yearOffset,
+    required this.summary,
+    required this.happinessScore,
+    required this.financialBalance,
+    required this.highlight,
+  });
+
+  final String name;
+  final String prefecture;
+  final int yearOffset;
+  final String summary;
+  final double happinessScore;
+  final double financialBalance;
+  final String highlight;
+}
